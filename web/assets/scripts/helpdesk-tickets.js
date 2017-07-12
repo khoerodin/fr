246473eef0eee9e0443550ea75a8780dcdd93987
Daniel <daniel.retraubun@gmail.com>
2017-07-12 11:20:58 +0700

    update hlpdesk-tiket 12jul

--- web/assets/scripts/helpdesk-tickets.js
+++ web/assets/scripts/helpdesk-tickets.js
@@ -2,8 +2,9 @@
  * Created by mispc3 on 10/07/17.
  */
 
-$(document).on('click', '#data-tiket', function (e) {
+$(document).on('click', '.detail-tic', function (e) {
     $('#tiketModal').modal({show: true, backdrop: 'static'});
+    //console.log(e);
 });
 
 
@@ -17,3 +18,20 @@ $(document).on('keypress', '#enter', function (e) {
         return false;
     }
 });
+
+$('.ticket-status').select2({
+    theme: "bootstrap",
+    minimumResultsForSearch: -1,
+    placeholder: "Status tiket",
+    allowClear: true
+
+});
+
+$('.ticket-priority').select2({
+    theme: "bootstrap",
+    minimumResultsForSearch: -1,
+    placeholder: "Pilih prioritas tiket",
+    allowClear: true
+
+});
+

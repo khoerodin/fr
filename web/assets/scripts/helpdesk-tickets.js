98856a88fe674ad13a1cd7111df68f022737295d
Daniel <daniel.retraubun@gmail.com>
2017-07-12 14:08:23 +0700

    update hlpdesk-tiket chatbox

--- web/assets/scripts/helpdesk-tickets.js
+++ web/assets/scripts/helpdesk-tickets.js
@@ -4,20 +4,16 @@
 
 $(document).on('click', '.detail-tic', function (e) {
     $('#tiketModal').modal({show: true, backdrop: 'static'});
-    //console.log(e);
+
 });
 
+// $(document).on('click', '#chatHistory', function (e) {
+//     $('#send').modal({
+//         show: true,
+//         backdrop: 'static'});
+//
+// });
 
-
-$(document).on('keypress', '#enter', function (e) {
-    if (e.which === 13) {
-        //tampilUser();
-        // var isi = $(this).val();
-        // alert(isi);
-        // console.log(e);
-        return false;
-    }
-});
 
 $('.ticket-status').select2({
     theme: "bootstrap",
@@ -35,3 +31,12 @@ $('.ticket-priority').select2({
 
 });
 
+$(document).find('#chatMessage').on('keypress', function(e){
+    if ( e.which == 13 ) {
+        e.preventDefault();
+        var text = $(this).val();
+        var html = '<div class="direct-chat-info clearfix"><span class="direct-chat-name pull-left">Test</span><span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span></div><div class="direct-chat-msg right"><div class="direct-chat-text">'+text+'</div></div>';
+        $(document).find('#chatHistory').append(html);
+        $(this).val('');
+    }
+});

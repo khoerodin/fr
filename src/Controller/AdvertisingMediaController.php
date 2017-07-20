874be46edf06d6012d568ccbc3af5e9f123c4ed4
Khoerodin <khoerodin@bisnis.com>
2017-07-20 08:57:01 +0700

    master media dan master akun eksekutif

---
+++ src/Controller/AdvertisingMediaController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Bisnis\Controller;
+
+
+class AdvertisingMediaController extends AdminController
+{
+    public function indexAction()
+    {
+        $meta = [
+            'parentMenu' => 'Iklan',
+            'title' => 'Media',
+        ];
+
+        $data = [
+            'meta' => $meta
+        ];
+
+        return $this->view('advertising-media/index.twig', $data);
+    }
+}

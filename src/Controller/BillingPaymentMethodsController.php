60aa3eeb0484f2378453101e895924e4828cb2bc
Khoerodin <khoerodin@bisnis.com>
2017-07-20 11:23:29 +0700

    sebelum edisi terbit

---
+++ src/Controller/BillingPaymentMethodsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Bisnis\Controller;
+
+
+class BillingPaymentMethodsController extends AdminController
+{
+    public function indexAction()
+    {
+        $meta = [
+            'parentMenu' => 'Tagihan',
+            'title' => 'Metode Pembayaran',
+        ];
+
+        $data = [
+            'meta' => $meta
+        ];
+
+        return $this->view('billing-payment-methods/index.twig', $data);
+    }
+}

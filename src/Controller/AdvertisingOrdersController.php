1d0f1c00b82c560023c7532ef95860f2400f6b6b
Khoerodin <khoerodin@bisnis.com>
2017-07-25 16:23:01 +0700

    detail order

--- src/Controller/AdvertisingOrdersController.php
+++ src/Controller/AdvertisingOrdersController.php
@@ -39,4 +39,32 @@ class AdvertisingOrdersController extends AdminController
 
         return $this->view('advertising-orders/new.twig', $data);
     }
+
+    public function detailAction($orderId)
+    {
+        $meta = [
+            'parentMenu' => 'Iklan',
+            'title' => 'Order Iklan',
+        ];
+
+        $order = $this->request('advertising/orders/' . $orderId, 'get');
+        $order = json_decode($order->getContent(), true);
+
+        $representatives = $this->request('representatives', 'get');
+        $representatives = json_decode($representatives->getContent(), true)['hydra:member'];
+
+        $paymentMethod = $this->request('billing/payment-methods', 'get');
+        $paymentMethod = json_decode($paymentMethod->getContent(), true)['hydra:member'];
+
+        $data = [
+            'meta' => $meta,
+            'order' => $order,
+            'representatives' => $representatives,
+            'paymentMethod' => $paymentMethod,
+            'emiten' => $emiten,
+            'sektor' => $sektor
+        ];
+
+        return $this->view('advertising-orders/detail.twig', $data);
+    }
 }

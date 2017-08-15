43c201fc33557ee8a3c6d10ad6d5233b01af16f3
Khoerodin <khoerodin@bisnis.com>
2017-08-15 09:31:06 +0700

    edit menus

--- web/assets/scripts/scripts.js
+++ web/assets/scripts/scripts.js
@@ -66,61 +66,61 @@ $(document).on('keypress', 'input.login-form', function (e) {
     }
 });
 
-$(document).on('click', 'li.treeview > a', function () {
-    title = $(this).find('span').text();
-    category = $(this).attr('data-catagory');
-
-    menusByCategory(title, category);
-    console.log(title, category);
-});
-
-$(document).on('click', 'ol.breadcrumb li.parent-crumb', function () {
-    title = $(this).text();
-    category = $(this).attr('data-catagory');
-
-    menusByCategory(title, category);
-});
-
-function menusByCategory(title, category) {
-
-    $.ajax({
-        url: "/api/menus",
-        type: "POST",
-        data: {
-            category : category
-        },
-        beforeSend: function () {
-        },
-        success: function (data, textStatus, jqXHR) {
-            menus = '<div class="row">';
-            $.each(data, function (index, value) {
-                menus += '<div class="col-lg-3 col-xs-6 right-menu" data-link="/'+value.path+'">';
-                menus += '  <div class="small-box box-menu bg-red">';
-                menus += '      <div class="inner">';
-                menus += '          <h4>'+value.name+'</h4>';
-                menus += '          <p style="visibility: hidden">Lorem ipsum...</p>';
-                menus += '      </div>';
-                menus += '      <div class="icon">';
-                menus += '          <i class="'+value.iconCls+'"></i>';
-                menus += '      </div>';
-                menus += '      <span class="small-box-footer" style="visibility: hidden;">More <i class="fa fa-arrow-circle-right"></i></span>';
-                menus += '  </div>';
-                menus += '</div>';
-            });
-            menus += '</div>';
-
-            $('title').html(title);
-            $('section.content-header h1').html(title);
-            $('section.content').html(menus);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-
-        }
-    });
-
-}
-
-$(document).on('click', 'div.right-menu', function () {
-    var url = $(this).attr('data-link');
-    window.location.href = url;
-});
+// $(document).on('click', 'li.treeview > a', function () {
+//     title = $(this).find('span').text();
+//     category = $(this).attr('data-catagory');
+//
+//     menusByCategory(title, category);
+//     console.log(title, category);
+// });
+//
+// $(document).on('click', 'ol.breadcrumb li.parent-crumb', function () {
+//     title = $(this).text();
+//     category = $(this).attr('data-catagory');
+//
+//     menusByCategory(title, category);
+// });
+//
+// function menusByCategory(title, category) {
+//
+//     $.ajax({
+//         url: "/api/menus",
+//         type: "POST",
+//         data: {
+//             category : category
+//         },
+//         beforeSend: function () {
+//         },
+//         success: function (data, textStatus, jqXHR) {
+//             menus = '<div class="row">';
+//             $.each(data, function (index, value) {
+//                 menus += '<div class="col-lg-3 col-xs-6 right-menu" data-link="/'+value.path+'">';
+//                 menus += '  <div class="small-box box-menu bg-red">';
+//                 menus += '      <div class="inner">';
+//                 menus += '          <h4>'+value.name+'</h4>';
+//                 menus += '          <p style="visibility: hidden">Lorem ipsum...</p>';
+//                 menus += '      </div>';
+//                 menus += '      <div class="icon">';
+//                 menus += '          <i class="'+value.iconCls+'"></i>';
+//                 menus += '      </div>';
+//                 menus += '      <span class="small-box-footer" style="visibility: hidden;">More <i class="fa fa-arrow-circle-right"></i></span>';
+//                 menus += '  </div>';
+//                 menus += '</div>';
+//             });
+//             menus += '</div>';
+//
+//             $('title').html(title);
+//             $('section.content-header h1').html(title);
+//             $('section.content').html(menus);
+//         },
+//         error: function (jqXHR, textStatus, errorThrown) {
+//
+//         }
+//     });
+//
+// }
+//
+// $(document).on('click', 'div.right-menu', function () {
+//     var url = $(this).attr('data-link');
+//     window.location.href = url;
+// });

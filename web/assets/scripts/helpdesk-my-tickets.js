6efec06cff0fe20d5cd146438a7fee396190f94e
Daniel <daniel.retraubun@gmail.com>
2017-08-16 16:53:40 +0700

    update helpdesk-ticket 16-08-17

--- web/assets/scripts/helpdesk-my-tickets.js
+++ web/assets/scripts/helpdesk-my-tickets.js
@@ -46,12 +46,37 @@ function getTicketList() {
 
                             tr += '<td align="center"><div class="fa fa-close fa-2x" data-toggle="tooltip" data-placement="bottom" title="Closed" style="color: indianred"></div></td>';
 
+                        } else if (value.status === 'onprogress') {
+
+                            tr += '<td align="center"><div class="fa fa-pencil fa-2x" data-toggle="tooltip" data-placement="bottom" title="On Progress" style="color: gold"></div></td>';
+
+                        } else if (value.status === 'resolved') {
+
+                            tr += '<td align="center"><div class="fa fa-check fa-2x" data-toggle="tooltip" data-placement="bottom" title="Resolved" style="color: lawngreen"></div></td>';
+
                         }
-                        tr += '<td>'+value.priority+'</td>';
+
+                        if(value.priority === 'very_urgent') {
+
+                            tr += '<td align="center"><div class="fa fa-fighter-jet fa-2x" data-toggle="tooltip" data-placement="bottom" title="Very Urgent" style="color: red;"></div></td>'
+
+                        } else if (value.priority === 'urgent') {
+
+                            tr += '<td align="center"><div class="fa fa-car fa-2x" data-toggle="tooltip" data-placement="bottom" title="Urgent" style="color: orangered;"></div></td>'
+
+                        } else if (value.priority === 'normal') {
+
+                            tr += '<td align="center"><div class="fa fa-motorcycle fa-2x" data-toggle="tooltip" data-placement="bottom" title="Normal" style="color: orange"></div></td>';
+
+                        } else if (value.priority === 'low') {
+
+                            tr += '<td align="center"><div class="fa fa-blind fa-2x" data-toggle="tooltip" data-placement="bottom" title="Low" style="color: darkorange"></div></td>';
+                        }
+
                         tr += '<td>'+moment(value.createdAt).format('LLLL')+'</td>';
 
                         if(value.staff){
-                            tr += '<td><button data-id="' + value.id + '" class="detail-tic btn btn-default btn-xs btn-flat" title="KIRIM PESAN"><i class="fa fa-envelope"></i></button></td>';
+                            tr += '<td><button data-id="' + value.id + '" class="detail-my-tic btn btn-default btn-xs btn-flat" title="KIRIM PESAN"><i class="fa fa-envelope"></i></button></td>';
                         }
 
                         tr += '</tr>';
@@ -143,6 +168,7 @@ $(document).on('click', '#btnSave', function () {
     });
 });
 
+// Melihat detil tiket (BELUM DIPAKAI)
 $(document).on('click', '.detail-tic', function () {
     var id = $(this).data('id');
 
@@ -326,7 +352,25 @@ function getClosedTicketList() {
                         // tr += '<td>'+value.message+'</td>';
                         // tr += '<td class="text-danger">'+value.status+'</td>';
                         tr += '<td align="center"><div class="fa fa-close fa-2x" data-toggle="tooltip" data-placement="bottom" title="Closed" style="color: indianred"></div></td>';
-                        tr += '<td>'+value.priority+'</td>';
+
+                        if(value.priority === 'very_urgent') {
+
+                            tr += '<td align="center"><div class="fa fa-fighter-jet fa-2x" data-toggle="tooltip" data-placement="bottom" title="Very Urgent" style="color: darkgray;"></div></td>'
+
+                        } else if (value.priority === 'urgent') {
+
+                            tr += '<td align="center"><div class="fa fa-car fa-2x" data-toggle="tooltip" data-placement="bottom" title="Urgent" style="color: darkgray;"></div></td>'
+
+                        } else if (value.priority === 'normal') {
+
+                            tr += '<td align="center"><div class="fa fa-motorcycle fa-2x" data-toggle="tooltip" data-placement="bottom" title="Normal" style="color: darkgray"></div></td>';
+
+                        } else if (value.priority === 'low') {
+
+                            tr += '<td align="center"><div class="fa fa-blind fa-2x" data-toggle="tooltip" data-placement="bottom" title="Low" style="color: darkgray"></div></td>';
+                        }
+
+
                         tr += '<td>'+moment(value.createdAt).format('LLLL')+'</td>';
                         tr += '<td>';
                         tr += '</td>';
@@ -356,3 +400,245 @@ function getClosedTicketList() {
     });
 
 }
+
+//DETAIL TIKET-KU (Forum)
+$(document).on('click', '.detail-my-tic', function () {
+    var ticketId = $(this).closest('tr').attr('id');
+    $('#tiketModal .modal-body').attr('data-ticketid', ticketId);
+
+    var staffId = $(this).closest('tr').data('staff');
+    $('#tiketModal .modal-body').attr('data-staffid', staffId);
+
+    var clientId = $(this).closest('tr').data('client');
+    $('#tiketModal .modal-body').attr('data-clientid', clientId);
+
+    var staffUserId = $(this).closest('tr').data('staff-user');
+    $('#tiketModal .modal-body').attr('data-staff-userid', staffUserId);
+
+    var timeId = $(this).closest('tr').data('waktu');
+    $('.list-post-date').html(moment(timeId).format("D MMM 'YY - HH:mm a"));
+
+
+    getTicketData(ticketId);
+    $('#tiketModal').modal({show: true, backdrop: 'static'});
+
+
+
+});
+
+//SEND CHAT - KEYBOARD ENTER
+$(document).find('#chatMessage').on('keypress', function(e){
+
+    if ( e.which == 13 ) {
+        e.preventDefault();
+        $('#send').trigger('click');
+    }
+
+});
+
+//<----------------- IS TYPING A MESSAGE -------------->
+var textarea = $('#chatMessage');
+var typingStatus = $('#typing_on');
+var lastTypedTime = moment().format("ddd, D MMM YYYY, h:mm A");
+var typingDelayMillis = 4000;
+
+
+function refreshTypingStatus() {
+    if (!textarea.is(':focus') || textarea.val() == '' || new Date().getTime() - lastTypedTime.getTime() > typingDelayMillis) {
+        typingStatus.html('<span style="visibility: hidden">.</span>');
+    } else {
+        typingStatus.html('someone is typing...');
+    }
+}
+function updateLastTypedTime() {
+    lastTypedTime = new Date();
+}
+
+setInterval(refreshTypingStatus, 100);
+textarea.keypress(updateLastTypedTime);
+textarea.blur(refreshTypingStatus);
+//<----------------- END IS TYPING A MESSAGE -------------->
+
+//<----------------- DATE TIME MOMENT JS -------------->
+var currentDay = moment().format("D MMM YYYY");
+var html = '<div><span class="current-date">'+currentDay+'</span></div>';
+$(document).find('.current-date').append(html);
+
+
+var currentTime = moment().format("kk.mm a");
+var html = '<span class="current-time">&nbsp;'+currentTime+'</span>';
+$(document).find('.current-time').append(html);
+
+var forumChat = moment().format('LLLL');
+var html = '<span class="wkt direct-chat-timestamp pull-right">'+forumChat+'</span>';
+$(document).find('.wkt').append(html);
+//<----------------- END DATE TIME MOMENT JS -------------->
+
+//<----------------- GET DETIL TIKET-KU UNTUK TIKET RESPON-------------->
+function getTicketData(ticketId) {
+
+
+    var title = $('tbody#ticketList tr#'+ticketId+' td:nth-child(3)').text();
+    // var message = $('tbody[data-list="helpdesk/tickets"] tr#'+ticketId+' td:nth-child(6)').text();
+    var category = $('tbody#ticketList tr#'+ticketId+' td:nth-child(2)').text();
+    var momentPost = $('tbody#ticketList tr#'+ticketId+' td:nth-child(6)').text();
+
+    $('#tiketModal .listDetail #list-title').text(title);
+    $('#tiketModal .listDetail #list-tgl-post').text(momentPost);
+    // $('#tiketModal .list-msg').text(message);
+    $('#tiketModal .listDetail #list-cat').text(category);
+
+    // console.log(ticketId);
+
+    $.ajax({
+        url: '/api',
+        type: 'POST',
+        data: {
+            module: 'helpdesk/ticket-responses',
+            method: 'get',
+            params: [
+                {
+                    'ticket.id': ticketId
+                }
+            ]
+        },
+        success: function (data) {
+            var data = JSON.parse(data);
+            // console.log(data);
+            var finalData = data['hydra:member'];
+
+            var dataForum = '';
+
+            $.each(finalData, function (index, value) {
+
+                dataForum += '<div class="direct-chat-msg" data-id="/api/helpdesk/ticket-responses/'+value.id+'"  data-ticket="/api/helpdesk/tickets/'+value.ticket.id+'" data-client="/api/users/'+value.client.id+'" data-time="'+value.createdAt+'">';
+                dataForum += '<div class="direct-chat-info clearfix">';
+                dataForum += '<span class="direct-chat-name pull-left">'+value.client.fullname+'</span>';
+                dataForum += '<span class="wkt direct-chat-timestamp pull-right">'+moment(value.createdAt).format('LLLL')+'</span>';
+                dataForum += '</div>';
+                dataForum += '<img class="direct-chat-img" src="../img/user4-128x128.jpg" alt="message user image">';
+                dataForum += '<div class="direct-chat-text">'+value.message+'</div>';
+                dataForum += '</div>';
+
+            });
+
+
+            $('#chatHistory').html(dataForum); //tbody result
+
+        }
+    });
+}
+//<----------------- END DETIL TIKET-KU UNTUK TIKET RESPON -------------->
+
+//<----------------- POST TIKET DAN POST FORUM-------------->
+function postTicketData(responseFor, staff, ticket, client, message, time) {
+    var params = [
+        {
+            name: 'responseFor',
+            value: responseFor
+        },
+        {
+            name: 'staff',
+            value: staff
+        },
+        {
+            name: 'ticket',
+            value: ticket
+        },
+        {
+            name: 'client',
+            value: client
+        },
+        {
+            name: 'message',
+            value: message
+        },
+        {
+            name: 'time',
+            value: ''
+        }
+    ];
+
+    var parameter = [];
+    $.each(params, function (index, value) {
+        if (value.value) {
+            parameter.push(value);
+        }
+    });
+
+    $.ajax({
+        url: '/api',
+        type: 'POST',
+        data: {
+            module: 'helpdesk/ticket-responses',
+            method: 'post',
+            params: parameter
+        },
+        success: function (data) {
+            var data = JSON.parse(data);
+
+            if(data === 401) {
+
+            } else {
+                // if (ticket) {
+                //$("#replyMessage").val('');
+                var ticketId =  ticket.split("/").pop();
+                // }
+
+                var result = '<div class="direct-chat-msg" data-id="/api/helpdesk/ticket-responses/'+data.id+'"  data-ticket="/api/helpdesk/tickets/'+data.ticket.id+'" data-staff="/api/users/'+data.staff.user.id+'" data-time="'+data.createdAt+'"><div class="direct-chat-info clearfix"><span class="direct-chat-name pull-left">'+data.staff.user.fullname+'</span><span class="wkt direct-chat-timestamp pull-right">'+moment(data.createdAt).format('LLLL')+'</span></div><img class="direct-chat-img" src="../img/user4-128x128.jpg" alt="message user image"><div class="direct-chat-text">'+data.message+'</div></div>';
+                $('#chatHistory').append(result);
+                $('#chatMessage').val('');
+            }
+        }
+    });
+}
+//<----------------- END POST TIKET DAN POST FORUM-------------->
+
+//CHAT klik send button
+$(document).on('click', '#send', function () {
+    var waktu = moment().format("ddd, D MMM YYYY, h:mm A");
+    var text = $('#chatMessage').val();
+    //var html = '<div class="direct-chat-info clearfix"><span class="direct-chat-name pull-left">Nama</span><span class="direct-chat-timestamp pull-right">'+waktu+'</span></div><div class="direct-chat-msg right"><div class="direct-chat-text">'+text+'</div></div>';
+    //$(document).find('#chatHistory').append(html);
+    var responseId = $('#chatHistory:last').data('id');
+    var staff = $('#chatHistory:last').data('staff');
+    var ticket = $('#chatHistory:last').data('ticket');
+    var client = $('#chatHistory:last').data('client');
+
+    var ticketId = $('#tiketModal .modal-body').data('ticketid');
+    var staffId = $('#tiketModal .modal-body').data('staffid');
+    var clientId = $('#tiketModal .modal-body').data('clientid');
+
+    var trueTicketId;
+    if(typeof ticket !== 'undefined'){
+        trueTicketId = ticket;
+    } else {
+        trueTicketId = '/api/helpdesk/tickets/'+ticketId;
+    }
+
+    var trueStaffId;
+    if(typeof staff !== 'undefined'){
+        trueStaffId = staff;
+    } else {
+        if (typeof staffId === 'undefined') {
+            trueStaffId = null;
+        } else {
+            trueStaffId = '/api/helpdesk/staffs/'+staffId;
+        }
+
+    }
+
+    var trueClientId;
+    if(typeof client !== 'undefined'){
+        trueClientId = client;
+    } else {
+        trueClientId = '/api/users/'+clientId
+    }
+
+    if(text !== ''){
+        postTicketData(responseId, trueStaffId, trueTicketId, trueClientId, text);
+    }else{
+        alert('Teks tidak boleh kosong bro..');
+    }
+
+});
--- web/assets/scripts/helpdesk-tickets.js
+++ web/assets/scripts/helpdesk-tickets.js
@@ -186,20 +186,22 @@ var html = '<span class="wkt direct-chat-timestamp pull-right">'+forumChat+'</sp
 $(document).find('.wkt').append(html);
 //<----------------- END DATE TIME MOMENT JS -------------->
 
-//<----------------- GET DETIL TIKET -------------->
+//<----------------- GET DETIL TIKET UNTUK TIKET RESPON-------------->
 function getTicketData(ticketId) {
 
-    var client = $('tbody[data-list="helpdesk/tickets"] tr#'+ticketId+' td:nth-child(2)').text();
-    var title = $('tbody[data-list="helpdesk/tickets"] tr#'+ticketId+' td:nth-child(5)').text();
+    var client = $('tbody#allTicketList tr#'+ticketId+' td:nth-child(2)').text();
+    var title = $('tbody#allTicketList tr#'+ticketId+' td:nth-child(5)').text();
     // var message = $('tbody[data-list="helpdesk/tickets"] tr#'+ticketId+' td:nth-child(6)').text();
-    var category = $('tbody[data-list="helpdesk/tickets"] tr#'+ticketId+' td:nth-child(4)').text();
-    var momentPost = $('tbody[data-list="helpdesk/tickets"] tr#'+ticketId+' td:nth-child(9)').text();
+    var category = $('tbody#allTicketList tr#'+ticketId+' td:nth-child(4)').text();
+    var momentPost = $('tbody#allTicketList tr#'+ticketId+' td:nth-child(8)').text();
 
-    $('#tiketModal .list-title').text(title);
-    $('#tiketModal .list-client').text(client);
-    $('#tiketModal .list-tgl-post').text(momentPost);
+    $('#tiketModal .listDetail #list-title').text(title);
+    $('#tiketModal .listDetail #list-client').text(client);
+    $('#tiketModal .listDetail #list-tgl-post').text(momentPost);
     // $('#tiketModal .list-msg').text(message);
-    $('#tiketModal .list-cat').text(category);
+    $('#tiketModal .listDetail #list-cat').text(category);
+
+    console.log();
 
     $.ajax({
         url: '/api',
@@ -215,7 +217,7 @@ function getTicketData(ticketId) {
         },
         success: function (data) {
             var data = JSON.parse(data);
-            console.log(data);
+            // console.log(data);
             var finalData = data['hydra:member'];
 
             var dataForum = '';
@@ -259,7 +261,7 @@ function getTicketData(ticketId) {
         }
     });
 }
-//<----------------- END DETIL TIKET -------------->
+//<----------------- END DETIL TIKET UNTUK TIKET RESPON -------------->
 
 //<----------------- POST TIKET DAN POST FORUM-------------->
 function postTicketData(responseFor, staff, ticket, client, message, time) {
@@ -308,7 +310,7 @@ function postTicketData(responseFor, staff, ticket, client, message, time) {
         success: function (data) {
             var data = JSON.parse(data);
 
-            if(data == 401) {
+            if(data === 401) {
 
             } else {
                 // if (ticket) {
@@ -317,6 +319,7 @@ function postTicketData(responseFor, staff, ticket, client, message, time) {
                 // }
 
                 var result = '<div class="direct-chat-msg" data-id="/api/helpdesk/ticket-responses/'+data.id+'"  data-ticket="/api/helpdesk/tickets/'+data.ticket.id+'" data-client="/api/users/'+data.client.id+'" data-time="'+data.createdAt+'"><div class="direct-chat-info clearfix"><span class="direct-chat-name pull-left">'+data.client.fullname+'</span><span class="wkt direct-chat-timestamp pull-right">'+moment(data.createdAt).format('LLLL')+'</span></div><img class="direct-chat-img" src="../img/user4-128x128.jpg" alt="message user image"><div class="direct-chat-text">'+data.message+'</div></div>';
+                console.log(result);
                 $('#chatHistory').append(result);
                 $('#chatMessage').val('');
             }
@@ -354,17 +357,52 @@ function getTicketList() {
                 var no = 1;
                 $.each(memberData, function (index, value) {
 
-                    if (value.status === 'closed') {
-
-                    } else if(value.status !== 'closed') {
-
-                        tr += '<tr>';
+                        tr += '<tr id="'+ value.id + '">';
                         tr += '<td>' + no + '</td>';
                         tr += '<td>' + value.category.name + '</td>';
                         tr += '<td>' + value.title + '</td>';
                         // tr += '<td>' + value.message + '</td>';
-                        tr += '<td>' + value.status + '</td>';
-                        tr += '<td>' + value.priority + '</td>';
+
+                        if(value.status === 'open') {
+
+                            tr += '<td align="center"><div class="fa fa-ticket fa-2x" data-toggle="tooltip" data-placement="bottom" title="Open" style="color: orange;"></div></td>'
+
+                        } else if (value.status === 'assignment') {
+
+                            tr += '<td align="center"><div class="fa fa-tag fa-2x" data-toggle="tooltip" data-placement="bottom" title="Assignment" style="color: cornflowerblue;"></div></td>'
+
+                        } else if (value.status === 'closed') {
+
+                            tr += '<td align="center"><div class="fa fa-close fa-2x" data-toggle="tooltip" data-placement="bottom" title="Closed" style="color: indianred"></div></td>';
+
+                        } else if (value.status === 'onprogress') {
+
+                            tr += '<td align="center"><div class="fa fa-pencil fa-2x" data-toggle="tooltip" data-placement="bottom" title="On Progress" style="color: gold"></div></td>';
+
+                        } else if (value.status === 'resolved') {
+
+                            tr += '<td align="center"><div class="fa fa-check fa-2x" data-toggle="tooltip" data-placement="bottom" title="Resolved" style="color: lawngreen"></div></td>';
+
+                        }
+
+                        if(value.priority === 'very_urgent') {
+
+                            tr += '<td align="center"><div class="fa fa-fighter-jet fa-2x" data-toggle="tooltip" data-placement="bottom" title="Very Urgent" style="color: red;"></div></td>'
+
+                        } else if (value.priority === 'urgent') {
+
+                            tr += '<td align="center"><div class="fa fa-car fa-2x" data-toggle="tooltip" data-placement="bottom" title="Urgent" style="color: orangered;"></div></td>'
+
+                        } else if (value.priority === 'normal') {
+
+                            tr += '<td align="center"><div class="fa fa-motorcycle fa-2x" data-toggle="tooltip" data-placement="bottom" title="Normal" style="color: orange"></div></td>';
+
+                        } else if (value.priority === 'low') {
+
+                            tr += '<td align="center"><div class="fa fa-blind fa-2x" data-toggle="tooltip" data-placement="bottom" title="Low" style="color: darkorange"></div></td>';
+                        }
+
+
                         tr += '<td>' + moment(value.createdAt).format('LLLL') + '</td>';
                         tr += '<td>';
 
@@ -377,7 +415,7 @@ function getTicketList() {
 
                         no++;
 
-                    }
+
                 });
 
             } else {
@@ -636,7 +674,24 @@ function getClosedTicketList() {
                         // tr += '<td>'+value.message+'</td>';
                         // tr += '<td>'+value.status+'</td>';
                         tr += '<td align="center"><div class="fa fa-close fa-2x" data-toggle="tooltip" data-placement="bottom" title="Closed" style="color: #d84747"></div></td>';
-                        tr += '<td>'+value.priority+'</td>';
+
+                        if(value.priority === 'very_urgent') {
+
+                            tr += '<td align="center"><div class="fa fa-fighter-jet fa-2x" data-toggle="tooltip" data-placement="bottom" title="Very Urgent" style="color: darkgray;"></div></td>';
+
+                        } else if (value.priority === 'urgent') {
+
+                            tr += '<td align="center"><div class="fa fa-car fa-2x" data-toggle="tooltip" data-placement="bottom" title="Urgent" style="color: darkgray;"></div></td>';
+
+                        } else if (value.priority === 'normal') {
+
+                            tr += '<td align="center"><div class="fa fa-motorcycle fa-2x" data-toggle="tooltip" data-placement="bottom" title="Normal" style="color: darkgray"></div></td>';
+
+                        } else if (value.priority === 'low') {
+
+                            tr += '<td align="center"><div class="fa fa-blind fa-2x" data-toggle="tooltip" data-placement="bottom" title="Low" style="color: darkgray"></div></td>';
+                        }
+
                         tr += '<td>'+moment(value.createdAt).format('LLLL')+'</td>';
                         tr += '<td>';
                         tr += '</td>';
@@ -717,9 +772,33 @@ function getMyAssignmentList() {
 
                             tr += '<td align="center"><div class="fa fa-close fa-2x" data-toggle="tooltip" data-placement="bottom" title="Closed" style="color: indianred"></div></td>';
 
+                        } else if (value.status === 'onprogress') {
+
+                            tr += '<td align="center"><div class="fa fa-pencil fa-2x" data-toggle="tooltip" data-placement="bottom" title="On Progress" style="color: gold"></div></td>';
+
+                        } else if (value.status === 'resolved') {
+
+                            tr += '<td align="center"><div class="fa fa-check fa-2x" data-toggle="tooltip" data-placement="bottom" title="Resolved" style="color: lawngreen"></div></td>';
+
                         }
 
-                        tr += '<td>'+value.priority+'</td>';
+                        if(value.priority === 'very_urgent') {
+
+                            tr += '<td align="center"><div class="fa fa-fighter-jet fa-2x" data-toggle="tooltip" data-placement="bottom" title="Very Urgent" style="color: red;"></div></td>'
+
+                        } else if (value.priority === 'urgent') {
+
+                            tr += '<td align="center"><div class="fa fa-car fa-2x" data-toggle="tooltip" data-placement="bottom" title="Urgent" style="color: orangered;"></div></td>'
+
+                        } else if (value.priority === 'normal') {
+
+                            tr += '<td align="center"><div class="fa fa-motorcycle fa-2x" data-toggle="tooltip" data-placement="bottom" title="Normal" style="color: orange"></div></td>';
+
+                        } else if (value.priority === 'low') {
+
+                            tr += '<td align="center"><div class="fa fa-blind fa-2x" data-toggle="tooltip" data-placement="bottom" title="Low" style="color: darkorange"></div></td>';
+                        }
+
                         tr += '<td>'+moment(value.createdAt).format('LLLL')+'</td>';
                         tr += '<td><button class="detail-tic btn btn-default btn-xs btn-flat" title="KIRIM PESAN"><i class="fa fa-envelope"></i></button></td>';
                         tr += '</tr>';
@@ -777,7 +856,7 @@ function getAllTicketList() {
 
                     if(value.status !== 'closed'){
 
-                        tr += '<tr>';
+                        tr += '<tr id="'+value.id+'">';
                         tr += '<td>' + no + '</td>';
 
                         if (value.client) {

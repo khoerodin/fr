0758213afe447332f5833adb102f7e1c1445d1ca
Khoerodin <khoerodin@bisnis.com>
2017-08-30 17:04:18 +0700

    tree categories

--- src/Controller/AdvertisingCategoriesController.php
+++ src/Controller/AdvertisingCategoriesController.php
@@ -11,10 +11,31 @@ class AdvertisingCategoriesController extends AdminController
             'title' => 'Kategori Iklan',
         ];
 
+        $categories = $this->request('advertising/categories', 'get');
+        $categories = json_decode($categories->getContent(), true)['hydra:member'];
+
         $data = [
-            'meta' => $meta
+            'meta' => $meta,
+            'categories' => $this->getCategory(NULL, $categories)
         ];
 
         return $this->view('advertising-categories/index.twig', $data);
     }
+
+    private function getCategory($parentCategoryId, $data) {
+        $str = '';
+        foreach ($data as $row) {
+            if ($row['parent']['id'] == $parentCategoryId) {
+                $str .= '<li><span data-id="' . $row['id'] . '" data-name="' . $row['name'] . '">' . $row['name'] . '</span>';
+                $res = $this->getCategory($row['id'], $data);
+                if ($res) {
+                    $str .= '<ul>' . $res . '</ul>';
+                }
+                $str .= '</li>';
+            }
+        }
+        return $str;
+
+    }
+
 }

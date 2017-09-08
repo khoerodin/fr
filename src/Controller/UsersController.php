e443a8984be74f38fa791b360e472150ec562be6
Khoerodin <khoerodin@bisnis.com>
2017-09-08 14:52:04 +0700

    perbaikan user roles

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -11,8 +11,17 @@ class UsersController extends AdminController
             'title' => 'Pengguna',
         ];
 
+        $services = $this->request('services', 'get');
+        $services = json_decode($services->getContent(), true)['hydra:member'];
+
+        // sorting ASC
+        usort($services, function($a, $b) {
+            return $a['name'] <=> $b['name'];
+        });
+
         $data = [
-            'meta' => $meta
+            'meta' => $meta,
+            'services' => $services
         ];
 
         return $this->view('users/index.twig', $data);

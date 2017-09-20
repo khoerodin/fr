8169368662be8f07ed08a4f882577cdac2185b78
Khoerodin <khoerodin@bisnis.com>
2017-09-20 09:02:06 +0700

    hilangkan buttons

--- web/assets/scripts/coba.js
+++ web/assets/scripts/coba.js
@@ -18,17 +18,15 @@ Bisnis.init(function () {
                 header: 'Email',
                 field: 'email',
                 type: 'text'
+            },
+            {
+                header: '<span class="pull-right">Aksi</span>',
+                custom: '<span class="pull-right">' +
+                '<button class="btn btn-xs btn-flat btn-default"><i class="fa fa-pencil"></i></button>' +
+                '<button class="btn btn-xs btn-flat btn-default"><i class="fa fa-times"></i></button>' +
+                '</span>',
+                width: '5%'
             }
-        ],
-        buttons: [
-            {
-                btnContent: '<i class="fa fa-pencil"></i>',
-                btnClass: 'btn btn-xs btn-flat btn-default'
-            },
-            {
-                btnContent: '<i class="fa fa-times"></i>',
-                btnClass: 'btn btn-xs btn-flat btn-default'
-            },
         ],
         search: {
             placeholder: 'CARI USERNAME / EMAIL',

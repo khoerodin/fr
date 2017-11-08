cd7bb2e3dfba28fa717e600b0e1613ee36b0dd05
Khoerodin <khoerodin@live.com>
2017-11-08 13:59:52 +0700

    untuk tarif khusus dkk, base price = 0

--- web/assets/scripts/Adv/OrderForm.js
+++ web/assets/scripts/Adv/OrderForm.js
@@ -133,9 +133,9 @@
     };
     // End Klien Iklan
 
-    // Cekat Di Faktur
+    // Cetak Di Faktur
     Bisnis.Util.Style.modifySelect('[name="printAs"]');
-    // End Cekat Di Faktur
+    // End Cetak Di Faktur
 
     // Jenis Iklan
     Bisnis.Util.Event.bind('click','#specificationButton', function () {
@@ -185,7 +185,6 @@
 
             document.querySelector('#typeName').value = '';
             document.querySelector('[name="type"]').value = '';
-            document.querySelector('[name="basePrice"]').value = '';
             document.querySelector('#typeButton').disabled = false;
 
             document.querySelector('[name="basePrice"]').value = '0';
@@ -275,11 +274,26 @@
                 function (dataResponse) {
                     var total = dataResponse['hydra:totalItems'];
                     var data = dataResponse['hydra:member'];
+                    var basePrice = document.querySelector('[name="basePrice"]');
 
                     if (total > 0) {
-                        document.querySelector('[name="basePrice"]').value = Bisnis.Util.Money.format(data[0]['price']);
+                        var jenisIklan = document.querySelector('#specificationName').value;
+                        jenisIklan = jenisIklan.toLowerCase().trim().replace(/\s+/g, '');
+                        var arrayKhusus = [
+                            'kuping',
+                            'banner',
+                            'stapel',
+                            'eksposisi',
+                            'tarifkhusus'
+                        ];
+
+                        if ( Bisnis.Util.Document.inArray(jenisIklan, arrayKhusus) ) {
+                            basePrice.value = 0;
+                        } else {
+                            basePrice.value = Bisnis.Util.Money.format(data[0]['price']);
+                        }
                     } else {
-                        document.querySelector('[name="basePrice"]').value = '0';
+                        basePrice.value = 0;
                     }
 
                     hitung();
